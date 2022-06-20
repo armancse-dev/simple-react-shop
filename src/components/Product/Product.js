e170bd5b4949dd4d5445d6b63ee5eeca62f8816f
armancse-dev <armanhossaincse7@gmail.com>
2022-06-20 20:25:29 +0600

    set eventhandler pass eventhandler to child

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,12 +1,37 @@
 import React from 'react';
-import './Product.css'
+import './Product.css';
+import { Card } from 'react-bootstrap'; 
+// import {Row} from 'react-bootstrap';
+// import {Col} from 'react-bootstrap';
+import {Button} from 'react-bootstrap';
 
-const Product = () => {
+const Product = (props) => {
+   // console.log(props);
+   const {name, img, donate, email,gender,id,age} = props.product;
    return (
-      <div>
+
+      <div className='product'>
          
+         <Card style={{ width: '18rem' }} >
+            <Card.Img variant="top" src={img} />
+            <Card.Body>
+               <Card.Title>Name:{name} </Card.Title>
+               <Card.Text>Age:{age}</Card.Text>
+               <Card.Text>Email: {email}</Card.Text>
+               <Card.Text>Gender: {gender}</Card.Text>
+               <Card.Text>Donate: {donate}</Card.Text>
+               <Button variant="primary" onClick={() => props.handleAddToCart(props.product)} className='btn-regular'>add to cart</Button>
+            </Card.Body>
+         </Card>
+
+         
+
       </div>
+
+      
    );
 };
+
+
 
 export default Product;
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,15 +1,36 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import Product from '../Product/Product';
 import './Shop.css'
 
 const Shop = () => {
+   const [products, setProducts] = useState([]);
+
    useEffect(() => {
       fetch('./tools.JSON')
       .then(res => res.json())
-      .then(data => console.log(data));
+      .then(data => setProducts(data));
    }, [])
+
+   const handleAddToCart = (product) => {
+      console.log(product);
+   }
+
    return (
-      <div>
-         
+      <div className='shop-container'>
+         <div className="product-container">
+            <h3>Products: {products.length}</h3>
+            {
+               products.map(product=> <Product 
+                  key={product.id}
+                  product={product}
+                  handleAddToCart = {handleAddToCart}
+                  ></Product> )
+            }
+         </div>
+         <div className="cart-container">
+            <h3>Alumni Added: </h3>
+            <h5>Total Donation:</h5>
+         </div>
       </div>
    );
 };
